1c55a7b5169b898662aa27056ea7891a8e83a93f
josephafern <joey.a.fern@gmail.com>
2021-10-06 16:42:54 -0700

    profile button added

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Modal from '../modal';
 import { Link } from 'react-router-dom';
-
+import { BsPersonSquare } from 'react-icons/bs';
 
 class Navbar extends React.Component {
     constructor(props){
@@ -35,7 +35,8 @@ class Navbar extends React.Component {
                     <button className={this.state.show && this.state.modal === 'signup' ? 'modal-btn clicked' : 'modal-btn'} onClick={this.showSignup}>Sign Up</button>
                     <button className={this.state.show && this.state.modal === 'login' ? 'modal-btn clicked' : 'modal-btn'} onClick={this.showLogin}>Sign In</button>
                 </div>
-                <div style={this.props.loggedIn !== null ? { display: 'block' } : { display: 'none' }}>
+                <div className={this.props.loggedIn !== null ? 'logout-section' : 'logout-section hidden'}>
+                    <Link to={`/users/${this.props.loggedIn}`}><BsPersonSquare className='profile-btn'/></Link>
                     <button id='nav-logout' className='modal-btn' onClick={this.props.logoutUser}>Log Out</button>
                 </div>
                 <Modal show={this.state.show} modal={this.state.modal} hideModal={this.hideModal} />
