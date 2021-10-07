5f47bc06aedcfbe03e651f8eeb256e19e2fbd304
josephafern <joey.a.fern@gmail.com>
2021-10-06 17:24:01 -0700

    added reviews to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_03_052005) do
+ActiveRecord::Schema.define(version: 2021_10_07_002036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,16 @@ ActiveRecord::Schema.define(version: 2021_10_03_052005) do
     t.datetime "updated_at", null: false
     t.integer "ratings", default: [], array: true
     t.string "price"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "restaurant_id", null: false
+    t.integer "rating", null: false
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
